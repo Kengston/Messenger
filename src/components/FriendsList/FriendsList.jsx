07919c3249a12413a464536b506510eb7ff50146
Kengston <danildc@mail.ru>
2020-10-07 17:22:56 +0300

    User-API added

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import fl from "./FriendsList.module.css"
 import * as axios from 'axios'
+import userPhoto from '../../img/icon_user.png';
 
 let FriendsList = (props) => {
 
     if (props.users.length === 0) {
 
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-
             props.setUsers(response.data.items)
         });
 
     }
 
     return (
-        <div>
+        <div className={fl.table}>
             {
                 props.users.map(u => <div key={u.id}>
 
                     <div className={fl.item}>
                         <div className={fl.picture}>
-                            <i className="fas fa-user-circle" alt="ava"/>
+                            {u.photos.small ? <img src={u.photos.small} className="fas fa-user-circle" alt="ava"/> : <i className="fas fa-user-circle" alt="ava" />}
                         </div>
-                        <div className={fl.name}>{u.fullName}
+                        <div className={fl.name}> {u.name}
                             {u.followed
                                 ? <button className={fl.button} onClick={() => {
                                     props.unfollow(u.id)
