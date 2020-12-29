d36f0ca8c451b39f392738596a1552ab63d41909
Kengston <danildc@mail.ru>
2020-12-30 01:20:13 +0700

    App initialize

--- src/App.js
+++ src/App.js
@@ -4,26 +4,48 @@ import Navbar from "./components/Navbar/Navbar";
 import Dialog from "./components/Dialogs/Dialogs";
 import FriendListContainer from "./components/FriendsList/FriendListContainer";
 import Preferences from "./components/Preferences/Preferences";
-import {Route} from "react-router-dom";
+import {withRouter, Route} from "react-router-dom";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
+import {connect} from "react-redux";
+import {compose} from "redux";
+import {initializeApp} from "./redux/app-reducer";
+import Preloader from "./components/common/Preloader/Preloader";
 
-const App = (props) => {
+class App extends React.Component {
 
-    return (
+    componentDidMount() {
+        this.props.initializeApp();
+    }
+
+    render() {
+        if (!this.props.initialized) {
+        return <Preloader />
+        }
+
+        return (
             <div className='app-wrapper'>
-                <HeaderContainer />
-                <Navbar />
+                <HeaderContainer/>
+                <Navbar/>
                 <div className='content'>
-                    <Route path='/Profile/:userId?' render={ () => <ProfileContainer state={props.state.profilePage}/> }/>
-                    <Route path='/Dialogs' render={ () => <Dialog state={props.state.dialogsPage}/> }/>
-                    <Route path='/FriendsList' render={ () => <FriendListContainer state={props.state.usersPage}/> }/>
-                    <Route path='/Preferences' component={Preferences} />
-                    <Route path='/Login' render={ () => <Login /> } />
+                    <Route path='/Profile/:userId?'
+                           render={() => <ProfileContainer state={this.props.state.profilePage}/>}/>
+                    <Route path='/Dialogs' render={() => <Dialog state={this.props.state.dialogsPage}/>}/>
+                    <Route path='/FriendsList'
+                           render={() => <FriendListContainer state={this.props.state.usersPage}/>}/>
+                    <Route path='/Preferences' component={Preferences}/>
+                    <Route path='/Login' render={() => <Login/>}/>
                 </div>
             </div>
-    );
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    initialized: state.app.initialized
+})
+
+export default compose(
+    withRouter,
+    connect(mapStateToProps, {initializeApp})) (App);
